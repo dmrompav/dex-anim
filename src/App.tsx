8d4f91f093aef2a68fc3557e9de95a801e5ad268
Dmitrii Pavlov <dm.rom.pav@gmail.com>
2025-09-01 20:37:30 +0500

    first init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.scss";
+
+export const App = () => {
+  return (
+    <>
+      <div className="wrapper">
+        <header className="h-12 flex space-between container mx-auto bg-red-500">
+          All bots
+        </header>
+        <main></main>
+      </div>
+    </>
+  );
+};
