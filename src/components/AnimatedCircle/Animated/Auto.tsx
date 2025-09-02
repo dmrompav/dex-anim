5df2e03c65f12d272f0290ebf97610354a8751aa
Dmitrii Pavlov <dm.rom.pav@gmail.com>
2025-09-02 22:01:52 +0500

    base layout

--- src/components/AnimatedCircle/Animated/Auto.tsx
+++ src/components/AnimatedCircle/Animated/Auto.tsx
@@ -3,41 +3,41 @@ import { Svg } from "../../Svg/Svg";
 import cls from "./Auto.module.scss";
 import autoSvg from "./assets/auto.svg?raw";
 
+const STEPS = [0, 400, 800, 1200, 1600, 2400];
+const CLASSES = [
+  cls.Step_1,
+  cls.Step_2,
+  cls.Step_3,
+  cls.Step_4,
+  cls.Step_5,
+  cls.Step_6,
+];
+
 export const Auto = () => {
   const ref = useRef<HTMLDivElement>(null);
+  const timers = useRef<number[]>([]);
+
+  const setOnly = (el: HTMLElement, kls: string) => {
+    CLASSES.forEach((c) => el.classList.remove(c));
+    el.classList.add(kls);
+  };
 
   const animate = () => {
-    setTimeout(() => {
-      ref.current?.classList.remove(cls.Step_6);
-      ref.current?.classList.add(cls.Step_1);
-    }, 0);
-    setTimeout(() => {
-      ref.current?.classList.remove(cls.Step_1);
-      ref.current?.classList.add(cls.Step_2);
-    }, 1000);
-    setTimeout(() => {
-      ref.current?.classList.remove(cls.Step_2);
-      ref.current?.classList.add(cls.Step_3);
-    }, 2000);
-    setTimeout(() => {
-      ref.current?.classList.remove(cls.Step_3);
-      ref.current?.classList.add(cls.Step_4);
-    }, 3000);
-    setTimeout(() => {
-      ref.current?.classList.remove(cls.Step_4);
-      ref.current?.classList.add(cls.Step_5);
-    }, 4000);
-    setTimeout(() => {
-      ref.current?.classList.remove(cls.Step_5);
-      ref.current?.classList.add(cls.Step_6);
-    }, 5000);
+    const el = ref.current;
+    if (!el) return;
+    timers.current.forEach(clearTimeout);
+    timers.current = [];
+
+    CLASSES.forEach((kls, i) => {
+      timers.current.push(window.setTimeout(() => setOnly(el, kls), STEPS[i]));
+    });
   };
 
   useEffect(() => {
     animate();
     const interval = setInterval(() => {
       animate();
-    }, 6000);
+    }, 2800);
     return () => clearInterval(interval);
   }, []);
 
